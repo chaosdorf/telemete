b7b1d84ffb11f78eb85d7157f529bdbc77df293b
mkuegler <kuegler_markus@web.de>
2018-12-14 22:47:03 +0100

    Add functionality to /list

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackQueryHandler
-from telegram import InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from valuables import API_KEY, BASE_ADDRESS
+import requests
+import json
 
 updater = Updater(token=API_KEY)
 dispatcher = updater.dispatcher
@@ -9,7 +11,14 @@ def commandStart(bot, update): # Startup and help message
     bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
 
 def commandList(bot, update): # Display available drinks
-    bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
+    drink_list = json.loads(requests.get(f"http://{BASE_ADDRESS}/api/v1/drinks.json").text)
+
+    output = "Available drinks:\n"
+
+    for drink in drink_list:
+        output += "\n{}: _{}€_".format(drink['name'], drink['price'])
+
+    bot.sendMessage(chat_id=update.message.chat_id, text=output, parse_mode=ParseMode.MARKDOWN)
 
 def commandBuy(bot, update): # Display available drinks as buttons and charge user accordingly
     bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
