9848c19ad2b18a0df08a9dd97c2317672a7db7d8
mkuegler <kuegler_markus@web.de>
2018-12-14 21:49:31 +0100

    Rename apikey.py to valuables.py (File itself not commited for security reasons) and add BASE_ADDRESS

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackQueryHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from apikey import API_KEY
+from valuables import API_KEY, BASE_ADDRESS
 
 updater = Updater(token=API_KEY)
 dispatcher = updater.dispatcher
