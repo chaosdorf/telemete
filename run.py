a4d56423898717a655aae8e4755f16d996e0eabc
mkuegler <kuegler_markus@web.de>
2018-12-14 22:19:07 +0100

    Implement basic functional structure

--- run.py
+++ run.py
@@ -5,7 +5,23 @@ from valuables import API_KEY, BASE_ADDRESS
 updater = Updater(token=API_KEY)
 dispatcher = updater.dispatcher
 
+def commandStart(bot, update): # Startup and help message
+    bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
 
+def commandList(bot, update): # Display available drinks
+    bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
+
+def commandBuy(bot, update): # Display available drinks as buttons and charge user accordingly
+    bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
+
+def commandBalance(bot, update): # Display current balance of user
+    bot.sendMessage(chat_id=update.message.chat_id, text="We are online!")
+
+dispatcher.add_handler(CommandHandler('start', commandStart))
+dispatcher.add_handler(CommandHandler('help', commandStart))
+dispatcher.add_handler(CommandHandler('list', commandList))
+dispatcher.add_handler(CommandHandler('buy', commandBuy))
+dispatcher.add_handler(CommandHandler('balance', commandBalance))
 
 updater.start_polling()
 
