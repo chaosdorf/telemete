e63d3ee29eb48c92d50a0b4ec8c2659e233119d4
mkuegler <kuegler_markus@web.de>
2019-01-05 01:14:03 +0100

    Add comments to functions, cases, GET-requests and unintuitive code

--- run.py
+++ run.py
@@ -6,23 +6,37 @@ import requests
 import json
 import sqlite3
 
+# Create a file called valuables.py like below and place it in the same folder as this
+
+# valuables.py
+# API_KEY=the key from telegram's botfather (string)
+# BASE_ADDRESS=the address of your mete instance (string) // Sidenote: http://BASE_ADDRESS/, so don't include http here
+# INIT_ADMIN={'telegram_id': your telegram id (int), 'mete_id': your mete id (int), 'user_handle': your telegram handle (string)}
+
+
 updater = Updater(token=API_KEY)
 dispatcher = updater.dispatcher
 database = sqlite3.connect("user_data")
 cursor = database.cursor()
 
+# This table contains the link between telegram and mete ids, whether or not a user is admin and their @-handle for telegram
 cursor.execute('''CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, telegram_id INTEGER, mete_id INTEGER, admin INTEGER DEFAULT 0, user_handle TEXT)''')
+
+# Check if initial admin has been linked and perform link if not
 cursor.execute('''SELECT id FROM users WHERE telegram_id=?''', (INIT_ADMIN['telegram_id'],))
 if cursor.fetchone() is None:
     cursor.execute('''INSERT INTO users(telegram_id, mete_id, admin, user_handle) VALUES(?,?,?,?)''', (INIT_ADMIN['telegram_id'], INIT_ADMIN['mete_id'], 1, INIT_ADMIN['user_handle'],))
 database.commit()
 cursor.close()
 
+# Default Button Layout for the most important commands
 kb = [[KeyboardButton("/list"), KeyboardButton("/buy"), KeyboardButton("/balance"), KeyboardButton("/help")]]
-kb_helponly = [[KeyboardButton("/help")]]
+
+# Buttonlayout for non-registered users
+kb_newusers = [[KeyboardButton("/start"), KeyboardButton("/list")]]
 
 kb_markup = ReplyKeyboardMarkup(kb, resize_keyboard=True)
-kb_helponly_markup = ReplyKeyboardMarkup(kb_helponly, resize_keyboard=True)
+kb_newusers_markup = ReplyKeyboardMarkup(kb_newusers, resize_keyboard=True)
 
 def commandStart(bot, update): # Startup and help message
     mete_id = getMeteID(update.message.chat_id)
@@ -35,8 +49,8 @@ def commandStart(bot, update): # Startup and help message
         admin_handles = cursor.fetchall()
         for u in admin_handles:
             output += "@{}\n".format(u[0])
-        output += "Please have your mete id at the ready to speed up the process."
-        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_helponly_markup)
+        output += "Please have your mete id ready to speed up the process."
+        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_newusers_markup)
     else:
         output += "\n/list lists all available drinks and their prices.\n\n"
         output += "/balance shows your account balance.\n\n"
@@ -45,6 +59,7 @@ def commandStart(bot, update): # Startup and help message
         bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_markup)
 
 def commandList(bot, update): # Display available drinks
+    # Get a list of all drinks (list[dict()])
     drink_list = json.loads(requests.get(f"http://{BASE_ADDRESS}/api/v1/drinks.json").text)
 
     output = "Available drinks:\n"
@@ -58,8 +73,9 @@ def commandBuy(bot, update): # Display available drinks as buttons and charge us
     mete_id = getMeteID(update.message.chat_id)
     if mete_id is None:
         output = "You are not linked to a mete account!"
-        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_helponly_markup)
+        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_newusers_markup)
     else:
+        # Get a list of all drinks (list[dict()])
         drink_list = json.loads(requests.get(f"http://{BASE_ADDRESS}/api/v1/drinks.json").text)
         kb_drinks = list()
 
@@ -79,27 +95,29 @@ def commandBalance(bot, update): # Display current balance of user
     mete_id = getMeteID(update.message.chat_id)
     if mete_id is None:
         output = "You are not linked to a mete account!"
-        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_helponly_markup)
+        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_newusers_markup)
     else:
         balance = getBalance(mete_id)
         output = "Your balance is _{:.2f}€_".format(balance)
         bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_markup, parse_mode=ParseMode.MARKDOWN)
 
-def commandCancel(bot, update):
+def commandCancel(bot, update): # Cancel action and return to standard button layout
     mete_id = getMeteID(update.message.chat_id)
     if mete_id is None:
         output = "You are not linked to a mete account!"
-        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_helponly_markup)
+        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_newusers_markup)
     else:
         output = "This request has been cancelled."
         bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_markup)
 
-def handle_inlinerequest(bot, update): # Check request for account linking and act accordingly
+def handle_inlinerequest(bot, update): # Handle any inline requests to this bot
     query = update.inline_query
     sender_id = query.from_user.id
     database = sqlite3.connect("user_data")
     cursor = database.cursor()
 
+
+    # Inline requests can only be started by admins, check this
     cursor.execute('''SELECT admin FROM users WHERE telegram_id=?''', (sender_id,))
     isAdmin = cursor.fetchone()
 
@@ -113,8 +131,9 @@ def handle_inlinerequest(bot, update): # Check request for account linking and a
 
     results = list()
     input = query.query.split(" ")
-    if input[0] == "link":
+    if input[0] == "link": # Link the recipient of the message to the specified mete account (Needs to be confirmed by recipient)
         mete_id = int(input[1])
+        # Get a list of all users (list[dict()])
         mete_user_list = json.loads(requests.get(f"http://{BASE_ADDRESS}/api/v1/users.json").text)
 
         valid_user = False
@@ -137,7 +156,7 @@ def handle_inlinerequest(bot, update): # Check request for account linking and a
         kb_link_markup = InlineKeyboardMarkup(kb_link)
 
         results.append(InlineQueryResultArticle(id="0", title="Send link request", input_message_content=InputTextMessageContent(output, parse_mode=ParseMode.MARKDOWN), reply_markup=kb_link_markup))
-    elif input[0] == "promote":
+    elif input[0] == "promote": # Promote the recipient of the message to be an administrator (Needs to be confirmed by recipient)
         output = "Press 'Become administrator' to become a Chaosdorf-Mete administrator."
         kb_admin_requests = [[InlineKeyboardButton("Become administrator", callback_data="promote")], [InlineKeyboardButton("Cancel", callback_data="cancel")]]
         kb_admin_requests_markup = InlineKeyboardMarkup(kb_admin_requests)
@@ -165,7 +184,7 @@ def handle_buttonpress(bot, update): # Handle any inline buttonpresses related t
             answer = "Success!"
         database.commit()
         cursor.close()
-    elif data[0] == "promote":
+    elif data[0] == "promote": # Confirm the promotion of users
         a = 0
         abort = False
         user = query.from_user
@@ -193,23 +212,26 @@ def handle_buttonpress(bot, update): # Handle any inline buttonpresses related t
 
                 output = "Successfully promoted this user to administrator!"
                 output = "Success!"
-    elif data[0] == "cancel":
+    elif data[0] == "cancel": # Cancel inline requests
         output = "This request has been cancelled."
         answer = "Cancelled!"
     bot.edit_message_text(output, inline_message_id=query.inline_message_id, parse_mode=ParseMode.MARKDOWN)
     bot.answer_callback_query(query.id, text=answer)
 
-def handle_textinput(bot, update):
+def handle_textinput(bot, update): # Handle any non-command text input to this bot
     mete_id = getMeteID(update.message.chat_id)
     if mete_id is None:
         output = "You are not linked to a mete account!"
-        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_helponly_markup, parse_mode=ParseMode.MARKDOWN)
+        bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_newusers_markup, parse_mode=ParseMode.MARKDOWN)
         return
     input = update.message.text
     splitup_input = input.split(":")
-    if len(splitup_input) == 2:
+    if len(splitup_input) == 2: # Check whether the input specifies a valid drink and purchase the drink
+        # input looks like this: 'name: x.xx€'. name can be carried over, for price the space and the € need to be removed, hence the slicing [1:-1] (everything but first and last char)
         name, price = splitup_input[0], splitup_input[1][1:-1]
         abort = True
+
+        # Get a list of all drinks (list[dict()])
         drink_list = json.loads(requests.get(f"http://{BASE_ADDRESS}/api/v1/drinks.json").text)
 
         for drink in drink_list:
@@ -218,6 +240,7 @@ def handle_textinput(bot, update):
                 abort = False
                 break
         if not abort:
+            # Buy a drink via http request
             requests.get("http://{}/api/v1/users/{}/buy?drink={}".format(BASE_ADDRESS, mete_id, drink_id))
             output = "You purchased _{}_. Your new balance is _{:.2f}€_".format(name, getBalance(mete_id))
             bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_markup, parse_mode=ParseMode.MARKDOWN)
@@ -225,7 +248,7 @@ def handle_textinput(bot, update):
     output = "Your input confused me. Get some /help"
     bot.sendMessage(chat_id=update.message.chat_id, text=output, reply_markup=kb_markup, parse_mode=ParseMode.MARKDOWN)
 
-def getMeteID(telegram_id):
+def getMeteID(telegram_id): # Returns the mete id linked to the specified telegram id or None, if there is no link
     database = sqlite3.connect("user_data")
     cursor = database.cursor()
 
@@ -236,7 +259,8 @@ def getMeteID(telegram_id):
     else:
         return mete_id[0]
 
-def getBalance(mete_id):
+def getBalance(mete_id): # Returns the specified user's balance as a float (Only used by other functions, validity of the mete id needs to be checked prior to calling this!)
+    # Get a list of all users (list[dict()])
     mete_user_list = json.loads(requests.get(f"http://{BASE_ADDRESS}/api/v1/users.json").text)
     for user in mete_user_list:
         if user['id'] == mete_id:
